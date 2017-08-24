a7b1058dd8af16b89136e1efd4ae208d43c15d27
Aktorius <miri.tarik@gmail.com>
2017-08-24 15:09:15 +0100

    Add EF package reference to web project

--- BillingManagement.Web/Controllers/HomeController.cs
+++ BillingManagement.Web/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 ﻿using System.Web.Mvc;
+using BillingManagement.Web.Services;
 
 namespace BillingManagement.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICompanyService _companyService;
+
+        public HomeController()
+            : this(new CompanyService()) { }
+
+        public HomeController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var companies = _companyService.GetAllCompanies();
+            return View(companies);
         }
 
         public ActionResult About()
