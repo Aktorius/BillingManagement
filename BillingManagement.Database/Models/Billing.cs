90e419be360eef0fa17977c6f93ef288528b933a
Aktorius <miri.tarik@gmail.com>
2017-08-24 13:40:09 +0100

    Create models

---
+++ BillingManagement.Database/Models/Billing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BillingManagement.Database.Models
+{
+    public class Billing
+    {
+        [Key]
+        public int BillingId { get; set; }
+        public string Notes { get; set; }
+
+        [Required]
+        public string BillingPhone { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime2")]
+        public DateTime DateFrom { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime2")]
+        public DateTime DateTo { get; set; }
+
+        public int SiteKey { get; set; }
+
+        [ForeignKey("SiteKey")]
+        public virtual Site Site { get; set; }
+    }
+}
---
+++ BillingManagement.Database/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BillingManagement.Database.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Site> Sites { get; set; }
+    }
+}
+}
